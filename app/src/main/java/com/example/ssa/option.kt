f114d3191ff64276cd51e3b7a5170f0e20db1b05
Hirano Tatsuya <k017c1066@it-neec.jp>
2019-05-23 15:45:31 +0900

    全てのファイルの追加
    プロジェクトの初期化。
    下記画面の作成。
    聞く : listen
    話す : talk
    オプション : option
    メイン : main
    書く : write
    ログイン : login

---
+++ app/src/main/java/com/example/ssa/option.kt
@@ -0,0 +1,12 @@
+package com.example.ssa
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class option : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_option)
+    }
+}
