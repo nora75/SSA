f9f2defef25dd9f3e28db10f98c7d582dd806291
Hirano Tatsuya <k017c1066@it-neec.jp>
2019-05-28 13:51:14 +0900

    各ボタンへのリスナの追加。遷移が可能に。関数toggleTalk()(仮)の作成。
    遷移のボタンには、各画面に遷移出来るようにした。
    録音ボタンにはリスナを設定し、toggleTalk()の呼び出し。
    録音中のテキストの表示がされるようになっている。
    また、押された事がわかるようにトーストの表示がされるようになっている。
    toggleTalk()では、押されたら録音ボタンの背景が変更になるようになっている。

--- app/src/main/java/com/example/ssa/MainActivity.kt
+++ app/src/main/java/com/example/ssa/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.ssa
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener{
+            val intent = Intent(this,talk::class.java)
+            startActivity(intent)
+        }
     }
+
 }
+
--- app/src/main/java/com/example/ssa/talk.kt
+++ app/src/main/java/com/example/ssa/talk.kt
@@ -1,12 +1,37 @@
 package com.example.ssa
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_talk.*
 
 class talk : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_talk)
+
+        to_listen.setOnClickListener {
+            val listen = Intent(this,listen::class.java)
+//            setContentView(R.layout.activity_listen)
+            startActivity(listen)
+        }
+
+        to_write.setOnClickListener {
+            setContentView(R.layout.activity_write)
+        }
+
+        talk_button.setOnClickListener {
+            toggleTalk()
+            Toast.makeText(this, "テストメッセージです", Toast.LENGTH_SHORT).show()
+            alert_talk.setVisibility(View.VISIBLE)
+        }
+
+    }
+
+    private fun toggleTalk() {
+        talk_button.setBackgroundColor(244000000)
     }
 }
