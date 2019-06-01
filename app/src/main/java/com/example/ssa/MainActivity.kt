e6b88e5a3bb8445a196581c8a8f4bec762c51cd5
TAKE1354 <k017c1354@it-neec.jp>
2019-06-01 20:49:07 +0900

    画面遷移と写真を撮れるところまで実装

--- app/src/main/java/com/example/ssa/MainActivity.kt
+++ app/src/main/java/com/example/ssa/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.ssa
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //書く画面への画面遷移用のリスナ―設定
+        Write_Button.setOnClickListener {
+            val write_intent = Intent(this, write::class.java)
+            startActivity(write_intent)
+        }
+
+        //オプション画面への画面遷移用のリスナー設定
+        Option_Button.setOnClickListener{
+            val option_intet = Intent(this,option::class.java)
+            startActivity(option_intet)
+        }
+
+        //オプション画面への画面遷移用のリスナー設定
+        Speak_Button.setOnClickListener{
+            val talk_intet = Intent(this,talk::class.java)
+            startActivity(talk_intet)
+        }
+
+        //オプション画面への画面遷移用のリスナー設定
+        Listen_See_Button.setOnClickListener{
+            val listen_intet = Intent(this,listen::class.java)
+            startActivity(listen_intet)
+        }
+
     }
 }
--- app/src/main/java/com/example/ssa/write.kt
+++ app/src/main/java/com/example/ssa/write.kt
@@ -1,12 +1,77 @@
 package com.example.ssa
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_write.*
 
 class write : AppCompatActivity() {
+
+    companion object {
+        private val CAMERA_REQUEST_CODE = 1
+        private val  CAMERA_PERMISSION_REQUEST_CODE = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        camera_button.setOnClickListener {
+            // カメラ機能を実装したアプリが存在するかチェック
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).resolveActivity(packageManager)?.let {
+                if (checkCameraPermission()) {
+                    takePicture()
+                } else {
+                    grantCameraPermission()
+                }
+            } ?: Toast.makeText(this, "カメラを扱うアプリがありません", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+                data?.extras?.get("data")?.let {
+                imageView.setImageBitmap(it as Bitmap)
+            }
+        }
+    }
+
+    private fun takePicture() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            addCategory(Intent.CATEGORY_DEFAULT)
+        }
+
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    }
+
+    private fun checkCameraPermission() = PackageManager.PERMISSION_GRANTED ==
+            ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+
+
+    private fun grantCameraPermission() =
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE)
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takePicture()
+            }
+        }
     }
 }
