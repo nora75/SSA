144f366115df0e0fab1d488d63b4f5f546b63dc4
AS-YDK <k017c1052@it-neec.jp>
2019-06-16 14:33:40 +0900

    DB

--- app/src/main/java/com/example/ssa/database.kt
+++ app/src/main/java/com/example/ssa/database.kt
@@ -5,39 +5,68 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DBHelper(var mContext : Context?) : SQLiteOpenHelper(mContext,"sampleDB",null,1) {
-    override fun onCreate(db: SQLiteDatabase) {
-        var user_id = null
-        var group_id = null
-        var flag = false
+abstract class DBHelper(var mContext : Context?) : SQLiteOpenHelper(mContext,"sampleDB",null,1) {
+    fun createTable(db: SQLiteDatabase) {
+        val sb = StringBuilder()
+        var sqlText : String
 
-        db.execSQL(
-            "CREATE TABLE User ("
-                    + user_id + " INT PRIMARY kEY,"
-                    + group_id + " INT,"
-                    + flag + " Boolean);"
-        )
-    }
+        sb.append("CREATE TABLE User (")
+        sb.append("user_id INT PRIMARY kEY,")
+        sb.append("group_id INT," )
+        sb.append("flag Boolean")
+        sb.append(");")
+        sqlText = sb.toString()
+        db.execSQL(sqlText)     //Userテーブルつくる
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        sb.append("CREATE TABLE Talk(")
+        sb.append("group_id TEXT")
+        sb.append("user_name TEXT")
+        sb.append("date DATE")
+        sb.append("audio_name text")
+        sb.append(");")
+        sqlText = sb.toString()
+        db.execSQL(sqlText)     //Talkテーブルつくる
+
+        sb.append("CREATE TABLE Diary(")
+        sb.append("group_id TEXT")
+        sb.append("user_name TEXT")
+        sb.append("date DATE")
+        sb.append("title TEXT")
+        sb.append("img_name TEXT")
+        sb.append("text TEXT")
+        sb.append(");")
+        sqlText = sb.toString()
+        db.execSQL(sqlText)     //Diaryテーブルつくる
     }
 }
 
 class Add(mContext: Context?){
-    private val db: SQLiteDatabase
-    private val DBH: DBHelper
-
-    init {
-        DBH = DBHelper(mContext)  // DB生成
-        db = DBH.getWritableDatabase()
-    }
-
-    fun addRecord(user_id : Int, group_id : Int, flag : Boolean){
+    fun addUserRecord(user_id : Int, group_id : Int, flag : Boolean){
         val values = ContentValues()
 
         values.put("user_id",user_id)
-        values.put("groupt_id",group_id)
+        values.put("group_id",group_id)
         values.put("flag",flag)
     }
+
+    fun addTalkRecord(group_id : String, user_name : String, date : Int, audio_name : String){
+        val values = ContentValues()
+
+        values.put("group_id",group_id)
+        values.put("user_name",user_name)
+        values.put("date",date)     //Date型対応してねぇ！
+        values.put("audio_name",audio_name)
+    }
+
+    fun addDiaryRecord(group_id : String, user_name : String, date : Int, title : String, img_name : String, text : String){
+        val values = ContentValues()
+
+        values.put("group_id",group_id)
+        values.put("user_name",user_name)
+        values.put("date",date)
+        values.put("title",title)
+        values.put("img_name",img_name)
+        values.put("text",text)
+    }
 }
+
