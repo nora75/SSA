304108282a7b9805d03aa000ef7059a0a10dd0f5
AS-YDK <k017c1052@it-neec.jp>
2019-07-04 23:44:18 +0900

    テーブル作成とデータ追加

--- app/src/main/java/com/example/ssa/Database.kt
+++ app/src/main/java/com/example/ssa/Database.kt
@@ -9,26 +9,15 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class Database(var mContext : Context?) : SQLiteOpenHelper(mContext,"insideDB",null,1) {
     override fun onCreate(db: SQLiteDatabase) {
-        val sb = StringBuilder()
-        var sqlText: String
+        db.execSQL(
+            "CREATE TABLE IF NOT EXISTS User (user_id INT PRIMARY KEY,user_name VARCHAR(10),mail VARCHAR(20),group_id VARCHAR(10));"
+        )
 
-        sb.append("CREATE TABLE User (")
-        sb.append("user_id INT PRIMARY kEY,")
-        sb.append("group_id INT,")
-        sb.append("flag Boolean")
-        sb.append(");")
-        sqlText = sb.toString()
-        db.execSQL(sqlText)     //Userテーブルつくる
+        db.execSQL(
+            "CREATE TABLE IF NOT EXISTS Data (date INT PRIMARY KEY,title VARCHAR(20),path_name VARCHAR(100),text VARCHAR(140),data_type INT);"
+        )
 
-        sb.append("CREATE TABLE Data(")
-        sb.append("date DATE")
-        sb.append("title TEXT")
-        sb.append("path_name TEXT")
-        sb.append("text TEXT")
-        sb.append("data_type INT")
-        sb.append(");")
-        sqlText = sb.toString()
-        db.execSQL(sqlText)
+        /* 改行入れるとダメっぽい */
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -36,76 +25,66 @@ class Database(var mContext : Context?) : SQLiteOpenHelper(mContext,"insideDB",n
     }
 }
 
-class DBController(mContext: Context){
-    val db: SQLiteDatabase
-    val DB : Database
+class DBController(mContext: Context) {
+    var db: SQLiteDatabase
+    var DB : Database
 
-    var result : CharArrayBuffer?
+    //var result: CharArrayBuffer?
 
-    init{
-        result = null
+    init {
+        //result = null
 
         DB = Database(mContext)
-        db = DB.writableDatabase    //DB作成!
+        db = DB.writableDatabase    //DB作成
     }
 
-    fun insertUserRecord(user_id : Int, user_name : String, mail : String, group_id : String){
-        val values = ContentValues()
-
-        values.put("user_id",user_id)
-        values.put("user_name",user_name)
-        values.put("mail",mail)
-        values.put("group_id",group_id)
-
-        db.insert("User",null,values)
-    }
-
-    fun insertDataRecord(date : Int, title : String, text : String, data_type : Int){
-        val values = ContentValues()
-
-        values.put("date",date)
-        values.put("title",title)
-        values.put("text",text)
-        values.put("data_type",data_type)   //data_type : 音声0,日記1
-
-        db.insert("Data",null,values)
-    }
-
-    fun getData() : RetArray{
-        var retDate : String = ""
-        var retTitle : String = ""
-        var retText : String = ""
-        var retDataType : String = ""
-
-        var data_type0 = 0
-        var data_type1 = 1
-
-        var sqlText = "SELECT * FROM Data WHERE data_type = ? or ?"
-
-        val c : Cursor = db.rawQuery(
-            sqlText,    //一度SQL文実行して
-            arrayOf(data_type0.toString(),data_type1.toString())    // data_type = 0 or 1で検索
-            /* これで全部吐き出す...ハズ */
+    fun insertUserRecord(user_id: Int, user_name: String, mail: String, group_id: String) {
+        db.execSQL(
+            "INSERT INTO User VALUES(" + user_id + "," + "'" + user_name + "','" + mail + "','" + group_id + "');"
         )
+    }
 
-        if(c.moveToNext()){     //次の行ある？
-            retDate = c.getString(c.getColumnIndex("date"))     // c.getColumnIndex()内の列を代入
-            retTitle = c.getString(c.getColumnIndex("title"))
-            retText = c.getString(c.getColumnIndex("text"))
-            retDataType = c.getString(c.getColumnIndex("data_type"))
-        }
-
-        c.close()   //クローズ大事
-
-        var result = RetArray(retDate,retTitle,retText,retDataType)
-
-        return result
+    fun insertDataRecord(date: Int, title: String,path_name: String, text: String, data_type: Int) {
+        db.execSQL(
+            "INSERT INTO Data VALUES(" + date + "," + "'" + title + "','" + path_name + "','" + text + "'," + data_type + ");"
+        )
     }
 }
-
-data class RetArray(
-    var date : String,
-    var title : String,
-    var text : String,
-    var datatype : String
-)
+//    fun getData() : RetArray{
+//        var retDate : String = ""
+//        var retTitle : String = ""
+//        var retText : String = ""
+//        var retDataType : String = ""
+//
+//        var data_type0 = 0
+//        var data_type1 = 1
+//
+//        var sqlText = "SELECT * FROM Data WHERE data_type = ? or ?"
+//
+//        val c : Cursor = db.rawQuery(
+//            sqlText,    //一度SQL文実行して
+//            arrayOf(data_type0.toString(),data_type1.toString())    // data_type = 0 or 1で検索
+//            /* これで全部吐き出す...ハズ */
+//        )
+//
+//        if(c.moveToNext()){     //次の行ある？
+//            retDate = c.getString(c.getColumnIndex("date"))     // c.getColumnIndex()内の列を代入
+//            retTitle = c.getString(c.getColumnIndex("title"))
+//            retText = c.getString(c.getColumnIndex("text"))
+//            retDataType = c.getString(c.getColumnIndex("data_type"))
+//        }
+//
+//        c.close()   //クローズ大事
+//
+//        var result = RetArray(retDate,retTitle,retText,retDataType)
+//
+//        return result
+//    }
+//}
+//
+//data class RetArray(
+//    var date : String,
+//    var title : String,
+//    var text : String,
+//    var datatype : String
+//)
--- app/src/main/java/com/example/ssa/MainActivity.kt
+++ app/src/main/java/com/example/ssa/MainActivity.kt
@@ -4,31 +4,30 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteException
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
+    lateinit var dbc : DBController
     override fun onCreate(savedInstanceState: Bundle?) {
+        dbc = DBController(this)
         try {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
 
-            val db: SQLiteDatabase? = Database(this).writableDatabase
-            // [つまりどころ]MainActivityがnullらしくてぬるぽ。SQLDatabase?にしてもぬるぽ
-            val dbc: DBController? = null
+            Database(this).writableDatabase
 
-            dbc?.insertUserRecord(0, "aaa", "AAA@hoge.com", "aaa")
-            dbc?.insertUserRecord(1, "bbb", "BBB@hoge.com", "bbb")
-            dbc?.insertUserRecord(2, "ccc", "CCC@hoge.com", "aaa")
+            dbc.insertUserRecord(1,  "aaa","aaa@hoge.com","abc")
+            dbc.insertUserRecord(2,  "bbb","bbb@hoge.com","def")
+            dbc.insertUserRecord(3,  "ccc","ccc@hoge.com","abc")
+
+            dbc.insertDataRecord(1,"aaa","hoge.png","hoge",0)
+            dbc.insertDataRecord(2,"bbb","piyo.png","piyo",1)
+            dbc.insertDataRecord(3,"ccc","fuga.png","fuga",1)
+
         }catch(err : SQLiteException){
             textView.setText(err.toString())
         }
     }
 }
-
-//data class PickData(
-//    var date : String,
-//    var title : String,
-//    var text : String,
-//    var datatype : String
-//)
