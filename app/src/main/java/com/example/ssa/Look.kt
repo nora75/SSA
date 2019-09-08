ae3ba714b52fae6304c00d0535e87e60283dfac4
TAKE1354 <k017c1354@it-neec.jp>
2019-09-09 03:24:40 +0900

    List表示終わり

--- app/src/main/java/com/example/ssa/Look.kt
+++ app/src/main/java/com/example/ssa/Look.kt
@@ -3,9 +3,6 @@ package com.example.ssa
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,9 +14,7 @@ import com.github.kittinunf.result.Result
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.android.synthetic.main.activity_look.*
-import java.io.BufferedReader
-import java.io.File
-import java.util.*
+import kotlinx.android.synthetic.main.my_text_view.view.*
 import android.widget.ListAdapter as ListAdapter1
 import com.example.ssa.ViewHolder as ViewHolder1
 import com.example.ssa.ViewHolder as ViewHolder2
@@ -28,14 +23,23 @@ class Look : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_look)
-        val items = Array(20,{i -> "Title-$i"})
+
         val names = listOf(
-            "サンプルA",
-            "サンプルB",
-            "サンプルC",
-            "サンプルD",
-            "サンプルE"
+            "おじいちゃん",
+            "おばあちゃん",
+            "ははおや",
+            "ちちおや",
+            "まご"
         )
+
+        val title = listOf(
+            "おはなみ",
+            "りょこう",
+            "しごと",
+            "やすみ",
+            "しゅうがくりょこう"
+        )
+
         val date = listOf(
             "9/2",
             "9/3",
@@ -43,46 +47,55 @@ class Look : AppCompatActivity() {
             "9/5",
             "9/6"
         )
-        val title = listOf(
-            "孫とあそんだよ",
-            "富士山に行ってきました",
-            "温泉旅行にいきました",
-            "お花見に行きました",
-            "家で寝てました"
-        )
-        val look = List(names.size){i ->SampleData(names[i],date[i],title[i])}
 
-        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items)
+        val test = List(names.size){i ->ProttypeData(names[i],title[i],date[i])}
+        val adapter = SampleListAdapter(this,test)
         myListView.adapter = adapter
+
+        myListView.setOnItemClickListener{adapterView,view,postion,id ->
+            val name = view.findViewById<TextView>(R.id.text1).text
+            Toast.makeText(this,"$name",Toast.LENGTH_LONG).show()
+        }
     }
 }
 
+data class ProttypeData(
+    val names:String,
+    val title:String,
+    val date: String
+)
 
-//
-/*class ListAdapter(context:Context,look: List<SampleData>) : ArrayAdapter<SampleData>(context,0,look){
+data class SampleViewHolder(
+    val text1: TextView,
+    val text2: TextView,
+    val text3: TextView
+)
+
+class SampleListAdapter(context: Context,sample:List<ProttypeData>) : ArrayAdapter<ProttypeData>(context,0,sample){
     private val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
         var view = convertView
-        var holder: com.example.ssa.ViewHolder
+        var holder : SampleViewHolder
 
         if(view == null){
-            view = layoutInflater.inflate(R.layout.my_text_view, parent,false)
-            holder = com.example.ssa.ViewHolder(
-
+            view = layoutInflater.inflate(R.layout.my_text_view, parent ,false)
+            holder = SampleViewHolder(
+                view.text1,
+                view.text2,
+                view.text3
             )
             view.tag = holder
-        }else{
-            holder = view.tag as com.example.ssa.ViewHolder
         }
+        else
+        {
+            holder = view.tag as SampleViewHolder
+        }
+        var sample = getItem(position) as ProttypeData
+        holder.text1.text = sample.names
+        holder.text2.text = sample.title
+        holder.text3.text = sample.date
 
-        var sample = getItem(position) as SampleData
-        holder.nameTextView.text = sample.names
-        holder.dateTextView.text = sample.date
-        holder.titleTextView.text = sample.title
-
-        return view!!
+        return view
     }
-
 }
-*/
