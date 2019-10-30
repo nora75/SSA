85e1fffffea654755fa277b25070ec9fc8f9dc4d
TAKE1354 <k017c1354@it-neec.jp>
2019-10-30 22:36:24 +0900

    マージ前

--- app/src/main/java/com/example/ssa/InDbAccess.kt
+++ app/src/main/java/com/example/ssa/InDbAccess.kt
@@ -6,7 +6,30 @@ import android.content.SharedPreferences
 
 
 class InDbAccess : Activity() {
-
     val dataStore: SharedPreferences = getSharedPreferences("USER_DATA", Context.MODE_PRIVATE)
+    val editor = dataStore.edit()
 
+    fun register(user_id:Int,group_id:String){
+        editor.putInt("USER_ID",user_id)
+        editor.putString("GROUP_ID",group_id)
+        editor.apply()
+    }
+
+    fun login(Address:String,password:String){
+        editor.putString("Address", Address)
+        editor.putString("Pass", password)
+        editor.apply()
+    }
+
+    fun write(){
+
+    }
+
+    fun listen(){
+
+    }
+
+    fun look(){
+
+    }
 }
