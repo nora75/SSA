24cae0ddbcd29c8b08079de148ff24547e45b29c
TAKE1354 <k017c1354@it-neec.jp>
2019-11-03 15:51:08 +0900

    アカウント情報の表示を完了

--- app/src/main/java/com/example/ssa/account_info.kt
+++ app/src/main/java/com/example/ssa/account_info.kt
@@ -1,12 +1,47 @@
 package com.example.ssa
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class account_info : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_info)
+
+        (findViewById(R.id.user_password) as TextView).setText(GetPass().toString())
+
+        (findViewById(R.id.user_name) as TextView).setText(GetName())
+
+        (findViewById(R.id.user_mailaddress) as TextView).setText(GetAddress())
+
+        (findViewById(R.id.user_group_id) as TextView).setText(GetGroupId())
     }
+    fun GetPass():String{
+        val dataStore: SharedPreferences = getSharedPreferences("USER_DATA", Context.MODE_PRIVATE)
+        val password = dataStore.getString("Pass","")
+        return password
+    }
+
+    fun GetName():String{
+        val dataStore: SharedPreferences = getSharedPreferences("USER_DATA", Context.MODE_PRIVATE)
+        val name = dataStore.getString("USER_NAME","")
+        return name
+    }
+
+    fun GetAddress():String{
+        val dataStore: SharedPreferences = getSharedPreferences("USER_DATA", Context.MODE_PRIVATE)
+        val mailaddress = dataStore.getString("Address","")
+        return mailaddress
+    }
+
+    fun GetGroupId():String{
+        val dataStore: SharedPreferences = getSharedPreferences("USER_DATA", Context.MODE_PRIVATE)
+        val groupid = dataStore.getString("GROUP_ID","")
+        return groupid
+    }
+
 }
