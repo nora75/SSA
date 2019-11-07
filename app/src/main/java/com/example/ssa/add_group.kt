c5d696f2c4c72eade6d4f97629812b5243f49851
TAKE1354 <k017c1354@it-neec.jp>
2019-11-07 09:34:12 +0900

    11/7朝

--- app/src/main/java/com/example/ssa/add_group.kt
+++ app/src/main/java/com/example/ssa/add_group.kt
@@ -2,11 +2,41 @@ package com.example.ssa
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kittinunf.fuel.httpPost
 
+/*
+*グループ変更機能
+* 使用用途
+* 間違えてGroup_idを入れずに登録してしまった場合に、入れ直す機能
+ */
+
+/*
+* メソッド
+* Change_group_id
+*
+*
+ */
 class add_group : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_group)
+
+    }
+
+
+    var Change = Change_group(
+        group_id = "",
+        password = ""
+    )
+
+    /**
+     *
+     */
+    fun Change_group_id(user_id:Int){
+        /*
+        "http://34.83.80.2:50112/users/$user_id"
+            .httpPost()
+         */
     }
 }
